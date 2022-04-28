33e0cc07f4acc8afd5865944de90bbd78d1f0d11
Rowan Manning <accounts+github@rowanmanning.com>
2022-04-28 17:23:12 +0100

    chore: add a temporary example package for testing

---
+++ packages/example/index.js
@@ -0,0 +1,9 @@
+
+// This is just an example module
+// and we will delete it before we
+// publish anything. It's required
+// in order to test Release Please
+
+module.exports = {
+	Hello: 'World'
+};
