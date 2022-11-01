515fdfd7a0954949c17f2cde1dc7e826c323c2b0
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2022-11-01 14:49:40 +0000

    fix: add proper types for the default logger

--- packages/logger/lib/index.js
+++ packages/logger/lib/index.js
@@ -1,12 +1,28 @@
 const Logger = require('./logger');
+const legacyMask = require('./transforms/legacy-mask');
 
+/**
+ * @typedef {object} Transforms
+ * @property {legacyMask} legacyMask
+ *     The legacy mask logger.
+ */
+
+/**
+ * @typedef {object} DefaultLogger
+ * @property {typeof Logger} Logger
+ *     The Logger class.
+ * @property {Transforms} transforms
+ *     Built-in log transforms.
+ */
+
+/**
+ * @type {Logger & DefaultLogger}
+ */
 module.exports = new Logger();
 
 module.exports.Logger = Logger;
 
-module.exports.transforms = {
-	legacyMask: require('./transforms/legacy-mask')
-};
+module.exports.transforms = { legacyMask };
 
 // @ts-ignore
 module.exports.default = module.exports;
