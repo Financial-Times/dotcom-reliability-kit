996468686dfc62db95e7bea4b2028a28dfb28621
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2022-11-01 09:22:31 +0000

    fix: correct ESM exports
    This commit 8cdd4d74afe44c51504ddf0f9a3219db3ad2654a broke the exports
    when used in a native ESM context. I'd rather have a little more clutter
    in our type definitions than not be able to import modules as our users
    expect. Thanks @apaleslimghost for the heads up.

--- packages/errors/lib/index.js
+++ packages/errors/lib/index.js
@@ -1,9 +1,7 @@
-module.exports = {
-	DataStoreError: require('./data-store-error'),
-	HttpError: require('./http-error'),
-	OperationalError: require('./operational-error'),
-	UpstreamServiceError: require('./upstream-service-error'),
-	UserInputError: require('./user-input-error')
-};
+exports.DataStoreError = require('./data-store-error');
+exports.HttpError = require('./http-error');
+exports.OperationalError = require('./operational-error');
+exports.UpstreamServiceError = require('./upstream-service-error');
+exports.UserInputError = require('./user-input-error');
 
-module.exports.default = module.exports;
+exports.default = exports;
