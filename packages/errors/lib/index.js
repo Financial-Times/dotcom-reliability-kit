Financial-Times/dotcom-reliability-kit

commit 8cdd4d74afe44c51504ddf0f9a3219db3ad2654a

fix: use module.exports for consistency
When we generate types using the `exports` object, it creates some empty
file-system-based namespaces which clutter things up. This ensures that
all of the module ".d.ts" files are consistent.

packages/errors/lib/index.js
@@ -1,7 +1,9 @@
-exports.DataStoreError = require('./data-store-error');
-exports.HttpError = require('./http-error');
-exports.OperationalError = require('./operational-error');
-exports.UpstreamServiceError = require('./upstream-service-error');
-exports.UserInputError = require('./user-input-error');
+module.exports = {
+	DataStoreError: require('./data-store-error'),
+	HttpError: require('./http-error'),
+	OperationalError: require('./operational-error'),
+	UpstreamServiceError: require('./upstream-service-error'),
+	UserInputError: require('./user-input-error')
+};
 
-exports.default = exports;
+module.exports.default = module.exports;

packages/log-error/lib/index.js
@@ -117,8 +117,10 @@ function logUnhandledError({ error, includeHeaders, request }) {
 	});
 }
 
-exports.logHandledError = logHandledError;
-exports.logRecoverableError = logRecoverableError;
-exports.logUnhandledError = logUnhandledError;
+module.exports = {
+	logHandledError,
+	logRecoverableError,
+	logUnhandledError
+};
 
-exports.default = exports;
+module.exports.default = module.exports;

commit 973a33c9759608589b053652c37ef586a82f1d25

fix: correct ESM exports
This commit 8cdd4d74afe44c51504ddf0f9a3219db3ad2654a broke the exports
when used in a native ESM context. I'd rather have a little more clutter
in our type definitions than not be able to import modules as our users
expect. Thanks @apaleslimghost for the heads up.

packages/errors/lib/index.js
@@ -1,9 +1,7 @@
-module.exports = {
-	DataStoreError: require('./data-store-error'),
-	HttpError: require('./http-error'),
-	OperationalError: require('./operational-error'),
-	UpstreamServiceError: require('./upstream-service-error'),
-	UserInputError: require('./user-input-error')
-};
+exports.DataStoreError = require('./data-store-error');
+exports.HttpError = require('./http-error');
+exports.OperationalError = require('./operational-error');
+exports.UpstreamServiceError = require('./upstream-service-error');
+exports.UserInputError = require('./user-input-error');
 
-module.exports.default = module.exports;
+exports.default = exports;

packages/log-error/lib/index.js
@@ -117,10 +117,8 @@ function logUnhandledError({ error, includeHeaders, request }) {
 	});
 }
 
-module.exports = {
-	logHandledError,
-	logRecoverableError,
-	logUnhandledError
-};
+exports.logHandledError = logHandledError;
+exports.logRecoverableError = logRecoverableError;
+exports.logUnhandledError = logUnhandledError;
 
-module.exports.default = module.exports;
+exports.default = exports;