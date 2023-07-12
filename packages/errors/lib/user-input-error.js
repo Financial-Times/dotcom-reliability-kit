b3d19859de8b7ef9521188722648bbcdf449dd6c
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-07-12 23:10:28 +0100

    fix: use overloads for error constructors
    This makes the types a lot easier to understand and it's much more clear
    when you try to pass invalid data into the error classes.

--- packages/errors/lib/user-input-error.js
+++ packages/errors/lib/user-input-error.js
@@ -13,8 +13,36 @@ class UserInputError extends HttpError {
 	name = 'UserInputError';
 
 	/**
-	 * Create a user input error.
+	 * Create an error with no arguments.
 	 *
+	 * @overload
+	 */
+	/**
+	 * Create an error with error data.
+	 *
+	 * @overload
+	 * @param {HttpError.HttpErrorData} data
+	 *     Additional error information.
+	 */
+	/**
+	 * Create an error with a message and optional error data.
+	 *
+	 * @overload
+	 * @param {string} message
+	 *     The error message.
+	 * @param {HttpError.HttpErrorData} [data]
+	 *     Additional error information.
+	 */
+	/**
+	 * Create an error with a status and optional error data.
+	 *
+	 * @overload
+	 * @param {number} status
+	 *     The error HTTP status code.
+	 * @param {HttpError.HttpErrorData} [data]
+	 *     Additional error information.
+	 */
+	/**
 	 * @param {string | number | HttpError.HttpErrorData} [message]
 	 *     The error message if it's a string, the HTTP status code if it's a number, or full error
 	 *     information if an object.
