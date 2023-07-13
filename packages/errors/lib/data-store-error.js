fecb0880ea7c38f89be144cf74935ccd5c19c8e9
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-07-13 09:42:44 +0100

    fix: simplify the DataStoreError class

--- packages/errors/lib/data-store-error.js
+++ packages/errors/lib/data-store-error.js
@@ -11,18 +11,6 @@ class DataStoreError extends OperationalError {
 	 * @type {string}
 	 */
 	name = 'DataStoreError';
-
-	/**
-	 * Create a data store error.
-	 *
-	 * @param {string | OperationalError.OperationalErrorData} [message]
-	 *     The error message if it's a string, or full error information if an object.
-	 * @param {OperationalError.OperationalErrorData} [data]
-	 *     Additional error information if `message` is a string.
-	 */
-	constructor(message, data = {}) {
-		super(message, data);
-	}
 }
 
 module.exports = DataStoreError;
