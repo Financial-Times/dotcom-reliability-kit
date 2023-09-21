3f7321c05ea39f49ab31baebcbb9415bf54c60da
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-09-21 11:49:52 +0100

    feat(wip)!: work in progress error rendering

--- packages/middleware-render-error-info/lib/index.js
+++ packages/middleware-render-error-info/lib/index.js
@@ -2,11 +2,20 @@ const appInfo = require('@dotcom-reliability-kit/app-info');
 const { logRecoverableError } = require('@dotcom-reliability-kit/log-error');
 const renderErrorPage = require('./render-error-page');
 const serializeError = require('@dotcom-reliability-kit/serialize-error');
+const { STATUS_CODES } = require('node:http');
+
+/**
+ * @typedef {"full" | "json" | "minimal"} RenderingStyle
+ */
 
 /**
  * @typedef {object} ErrorRenderingOptions
- * @property {import('@dotcom-reliability-kit/log-error').Logger & Object<string, any>} [logger]
+ * @property {import('@dotcom-reliability-kit/log-error').Logger & {[key: string]: any}} [logger]
  *     The logger to use to output errors. Defaults to Reliability Kit logger.
+ * @property {RenderingStyle} [renderer]
+ *     The renderer to use when outputting error information, one of "full", "json", or "minimal".
+ *     Defaults to "minimal" if the `NODE_ENV` environment variable is "production", otherwise
+ *     defaults to "full".
  */
 
 /**
@@ -19,16 +28,56 @@ const serializeError = require('@dotcom-reliability-kit/serialize-error');
  *     Returns error info rendering middleware.
  */
 function createErrorRenderingMiddleware(options = {}) {
-	// Only render the error info page if we're not in production.
-	const performRendering = appInfo.environment === 'development';
+	// Only render full error information if we're in production
+	const defaultRenderer =
+		appInfo.environment === 'development' ? 'full' : 'minimal';
+	const renderer = options.renderer || defaultRenderer;
 
 	return function errorRenderingMiddleware(error, request, response, next) {
-		if (performRendering) {
-			// It's unlikely that this will fail but we want to be sure
-			// that any rendering errors are caught properly
-			try {
-				const serializedError = serializeError(error);
-				response.status(serializedError.statusCode || 500);
+		// It's unlikely that this will fail but we want to be sure
+		// that any rendering errors are caught properly
+		try {
+			// If headers have been sent already then we need to hand off to
+			// the final Express error handler as documented here:
+			// https://expressjs.com/en/guide/error-handling.html#the-default-error-handler
+			//
+			// This ensures that the app doesn't crash with `ERR_STREAM_WRITE_AFTER_END`
+			if (response.headersSent) {
+				logRecoverableError({
+					error: Object.assign(
+						new Error(
+							'The error handler could not output because the response has already been sent'
+						),
+						{ code: 'ERROR_RENDERING_FAILURE' }
+					),
+					request
+				});
+				return next(error);
+			}
+
+			const serializedError = serializeError(error);
+
+			// Make sure we have a valid status code and then send it
+			const statusCode = isValidStatusCode(serializedError.statusCode)
+				? serializedError.statusCode
+				: 500;
+			response.status(statusCode);
+
+			// Render JSON errors if the rendering style is explicitly configured
+			if (renderer === 'json') {
+				response.set('content-type', 'application/json');
+				const jsonResponse = {
+					error: {
+						name: serializedError.name,
+						code: serializedError.code,
+						message: serializedError.message
+					}
+				};
+				return response.send(JSON.stringify(jsonResponse, null, '\t'));
+			}
+
+			// Render full debug pages in local development
+			if (renderer === 'full') {
 				response.set('content-type', 'text/html');
 				return response.send(
 					renderErrorPage({
@@ -37,18 +86,39 @@ function createErrorRenderingMiddleware(options = {}) {
 						serializedError
 					})
 				);
-			} catch (/** @type {any} */ renderingError) {
-				logRecoverableError({
-					error: renderingError,
-					logger: options.logger,
-					request
-				});
 			}
+
+			// Render a minimal error message if in production or if an unexpected
+			// rendering style is set.
+			response.set('content-type', 'text/plain');
+			return response.send(STATUS_CODES[statusCode] || 'Internal Server Error');
+		} catch (/** @type {any} */ renderingError) {
+			logRecoverableError({
+				error: renderingError,
+				logger: options.logger,
+				request
+			});
+			return next(renderingError);
 		}
-		next(error);
 	};
 }
 
-module.exports = createErrorRenderingMiddleware;
+/**
+ * @param {any} statusCode
+ *     The status code to check.
+ * @returns {statusCode is number}
+ *     Returns whether the status code is valid.
+ */
+function isValidStatusCode(statusCode) {
+	return (
+		statusCode &&
+		// The error status is a number
+		!Number.isNaN(statusCode) &&
+		// The error status is in range
+		statusCode >= 400 &&
+		statusCode < 600
+	);
+}
 
+module.exports = createErrorRenderingMiddleware;
 module.exports.default = module.exports;
