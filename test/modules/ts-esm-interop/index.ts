978e4e2e555013891a62bdd890cb8847b23ddeb4
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-02-05 08:30:40 +0000

    chore: add failing tests for serialize-request
    The types don't work at all if you're using serialize-request in ESM
    (any of the various flavours).

--- test/modules/ts-esm-interop/index.ts
+++ test/modules/ts-esm-interop/index.ts
@@ -1,6 +1,7 @@
 import logger1 from '@dotcom-reliability-kit/logger';
 import * as logger2 from '@dotcom-reliability-kit/logger';
 import { Logger, transforms } from '@dotcom-reliability-kit/logger';
+import serializeRequest from '@dotcom-reliability-kit/serialize-request';
 
 type TypeTests = {
 	logger1: Logger;
@@ -18,5 +19,9 @@ export default {
 new Logger({
 	transforms: [transforms.legacyMask()]
 });
+
+// Test that request serialization works.
+// See: https://github.com/Financial-Times/cp-content-pipeline/blob/90ce06158b65742cd03cbf03f5372790906cad9e/packages/api/src/plugins/logging.ts#L1-L3
+serializeRequest({ url: 'https://example.com' });
 
 console.log('OK');
