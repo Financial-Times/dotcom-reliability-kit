bbf50bceae65cbb93fe24efd5701c70728d2db6e
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-02-06 14:30:25 +0000

    chore: allow manual types to be opted into
    This means we can replace the generated types package-by-package instead
    of having to do them all at once.

---
+++ scripts/clean-generated-types.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+find ./packages/app-info -name "*.d.ts*" | xargs -r rm
+find ./packages/crash-handler -name "*.d.ts*" | xargs -r rm
+find ./packages/errors -name "*.d.ts*" | xargs -r rm
+find ./packages/eslint-config -name "*.d.ts*" | xargs -r rm
+find ./packages/fetch-error-handler -name "*.d.ts*" | xargs -r rm
+find ./packages/log-error -name "*.d.ts*" | xargs -r rm
+find ./packages/logger -name "*.d.ts*" | xargs -r rm
+find ./packages/middleware-log-errors -name "*.d.ts*" | xargs -r rm
+find ./packages/middleware-render-error-info -name "*.d.ts*" | xargs -r rm
+find ./packages/opentelemetry -name "*.d.ts*" | xargs -r rm
+find ./packages/serialize-error -name "*.d.ts*" | xargs -r rm
+find ./packages/serialize-request -name "*.d.ts*" | xargs -r rm
