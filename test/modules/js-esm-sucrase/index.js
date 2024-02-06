eeee9822e5d840f02f04fff0148c91af58a31cfe
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-02-06 12:06:48 +0000

    chore: add tests for serialize-error/app-info

--- test/modules/js-esm-sucrase/index.js
+++ test/modules/js-esm-sucrase/index.js
@@ -1,7 +1,15 @@
+import appInfo from '@dotcom-reliability-kit/app-info';
+import { environment } from '@dotcom-reliability-kit/app-info';
 import logger1 from '@dotcom-reliability-kit/logger';
 import * as logger2 from '@dotcom-reliability-kit/logger';
 import { Logger, transforms } from '@dotcom-reliability-kit/logger';
+import serializeError from '@dotcom-reliability-kit/serialize-error';
 import serializeRequest from '@dotcom-reliability-kit/serialize-request';
+
+// Test that appInfo types work
+if (appInfo.environment !== environment) {
+	throw new Error('appInfo is not working');
+}
 
 /**
  * @typedef {object} TypeTests
@@ -20,8 +28,9 @@ new Logger({
 	transforms: [transforms.legacyMask()]
 });
 
-// Test that request serialization works.
+// Test that error and request serialization works.
 // See: https://github.com/Financial-Times/cp-content-pipeline/blob/90ce06158b65742cd03cbf03f5372790906cad9e/packages/api/src/plugins/logging.ts#L1-L3
+serializeError(new Error('hi'));
 serializeRequest({ url: 'https://example.com' });
 
 console.log('OK');
--- test/modules/ts-commonjs-nointerop/index.ts
+++ test/modules/ts-commonjs-nointerop/index.ts
@@ -1,7 +1,15 @@
+const appInfo = require('@dotcom-reliability-kit/app-info');
+const { environment } = require('@dotcom-reliability-kit/app-info');
 const logger1 = require('@dotcom-reliability-kit/logger');
 const logger2 = require('@dotcom-reliability-kit/logger').default;
 const { Logger, transforms } = require('@dotcom-reliability-kit/logger');
+const serializeError = require('@dotcom-reliability-kit/serialize-error');
 const serializeRequest = require('@dotcom-reliability-kit/serialize-request');
+
+// Test that appInfo types work
+if (appInfo.environment !== environment) {
+	throw new Error('appInfo is not working');
+}
 
 type TypeTests = {
 	// @ts-ignore TODO this isn't working correctly and we'll need
@@ -27,8 +35,9 @@ new Logger({
 	transforms: [transforms.legacyMask()]
 });
 
-// Test that request serialization works.
+// Test that error and request serialization works.
 // See: https://github.com/Financial-Times/cp-content-pipeline/blob/90ce06158b65742cd03cbf03f5372790906cad9e/packages/api/src/plugins/logging.ts#L1-L3
+serializeError(new Error('hi'));
 serializeRequest({ url: 'https://example.com' });
 
 console.log('OK');
