b45d3e42cda481d898eadebf4aadab0e9b2db823
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-02-19 10:41:47 +0000

    fix: add type declarations for serialize-request
    This adds manual type declarations for serialize-request which should
    make it work in most projects. The tests are now passing.

--- scripts/clean-generated-types.sh
+++ scripts/clean-generated-types.sh
@@ -10,4 +10,3 @@ find ./packages/middleware-log-errors -name "*.d.ts*" | xargs -r rm
 find ./packages/middleware-render-error-info -name "*.d.ts*" | xargs -r rm
 find ./packages/opentelemetry -name "*.d.ts*" | xargs -r rm
 find ./packages/serialize-error -name "*.d.ts*" | xargs -r rm
-find ./packages/serialize-request -name "*.d.ts*" | xargs -r rm
