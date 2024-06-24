97311fd115f7e9921f3970c51a6b421aec385f01
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-06-24 15:37:34 +0100

    fix: add type declarations for log-error

--- packages/log-error/lib/index.js
+++ packages/log-error/lib/index.js
@@ -4,44 +4,14 @@ const serializeError = require('@dotcom-reliability-kit/serialize-error');
 const serializeRequest = require('@dotcom-reliability-kit/serialize-request');
 
 /**
- * @typedef {(...logData: any) => any} LogMethod
- */
-
-/**
- * @typedef {object} Logger
- * @property {LogMethod} error
- *     A function to log an error.
- * @property {LogMethod} [fatal]
- *     A function to log a fatal error.
- * @property {LogMethod} warn
- *     A function to log a warning.
- */
-
-/**
- * @typedef {object} ErrorLoggingOptions
- * @property {(string | Error & Record<string, any>)} error
- *     The error to log.
- * @property {string[]} [includeHeaders]
- *     An array of request headers to include in the log.
- * @property {Logger & {[key: string]: any}} [logger]
- *     The logger to use to output errors. Defaults to Reliability Kit logger.
- * @property {(string | import('@dotcom-reliability-kit/serialize-request').Request)} [request]
- *     An request object to include in the log.
- */
-
-/**
- * @typedef {object} InternalErrorLoggingOptions
- * @property {string} event
- *     The event to log.
- * @property {("error" | "fatal" | "warn")} level
- *     The log level to use. One of "error", "fatal", or "warn".
+ * @typedef {import('@dotcom-reliability-kit/log-error').ErrorLoggingOptions} ErrorLoggingOptions
  */
 
 /**
  * Log an error object with optional request information.
  *
  * @private
- * @param {ErrorLoggingOptions & InternalErrorLoggingOptions} options
+ * @param {ErrorLoggingOptions & {event: string, level: 'error' | 'fatal' | 'warn'}} options
  *     The data to log.
  * @returns {void}
  */
@@ -110,10 +80,7 @@ function extractErrorMessage(serializedError) {
 /**
  * Log a handled error.
  *
- * @public
- * @param {ErrorLoggingOptions} options
- *     The data to log.
- * @returns {void}
+ * @type {typeof import('@dotcom-reliability-kit/log-error').logHandledError}
  */
 function logHandledError({ error, includeHeaders, logger, request }) {
 	logError({
@@ -129,10 +96,7 @@ function logHandledError({ error, includeHeaders, logger, request }) {
 /**
  * Log a recoverable error.
  *
- * @public
- * @param {ErrorLoggingOptions} options
- *     The data to log.
- * @returns {void}
+ * @type {typeof import('@dotcom-reliability-kit/log-error').logHandledError}
  */
 function logRecoverableError({ error, includeHeaders, logger, request }) {
 	logError({
@@ -148,10 +112,7 @@ function logRecoverableError({ error, includeHeaders, logger, request }) {
 /**
  * Log an unhandled error.
  *
- * @public
- * @param {ErrorLoggingOptions} options
- *     The data to log.
- * @returns {void}
+ * @type {typeof import('@dotcom-reliability-kit/log-error').logHandledError}
  */
 function logUnhandledError({ error, includeHeaders, logger, request }) {
 	logError({
