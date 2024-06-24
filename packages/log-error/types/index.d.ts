d2c3f84790bf736212fd54638674eaeb15876007
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-06-24 15:37:34 +0100

    fix: add type declarations for log-error

---
+++ packages/log-error/types/index.d.ts
@@ -0,0 +1,24 @@
+import { Request } from '@dotcom-reliability-kit/serialize-request';
+
+declare module '@dotcom-reliability-kit/log-error' {
+	type LogMethod = (...logData: any) => any;
+
+	export type Logger = {
+		error: LogMethod;
+		fatal?: LogMethod;
+		warn: LogMethod;
+	};
+
+	export type ErrorLoggingOptions = {
+		error: string | (Error & Record<string, any>);
+		includeHeaders?: string[];
+		logger?: Logger & { [key: string]: any };
+		request?: string | Request;
+	};
+
+	export function logHandledError(options: ErrorLoggingOptions): void;
+
+	export function logRecoverableError(options: ErrorLoggingOptions): void;
+
+	export function logUnhandledError(options: ErrorLoggingOptions): void;
+}
--- scripts/clean-generated-types.sh
+++ scripts/clean-generated-types.sh
@@ -2,7 +2,6 @@
 
 find ./packages/errors -name "*.d.ts*" | xargs -r rm
 find ./packages/fetch-error-handler -name "*.d.ts*" | xargs -r rm
-find ./packages/log-error -name "*.d.ts*" | xargs -r rm
 find ./packages/logger -name "*.d.ts*" | xargs -r rm
 find ./packages/middleware-log-errors -name "*.d.ts*" | xargs -r rm
 find ./packages/middleware-render-error-info -name "*.d.ts*" | xargs -r rm
