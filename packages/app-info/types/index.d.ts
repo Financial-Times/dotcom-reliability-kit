02bfcf2525c6ad6716ad90a4641357beaca0692b
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-06-28 14:28:50 +0100

    chore: run prettier against types

--- packages/app-info/types/index.d.ts
+++ packages/app-info/types/index.d.ts
@@ -14,34 +14,34 @@ declare module '@dotcom-reliability-kit/app-info' {
 
 	export type SemanticConventions = {
 		cloud: {
-			provider?: string,
-			region?: string
-		},
+			provider?: string;
+			region?: string;
+		};
 		deployment: {
-			environment: string
-		},
+			environment: string;
+		};
 		service: {
-			name?: string
-			version?: string,
+			name?: string;
+			version?: string;
 			instance: {
-				id: string
-			}
-		}
+				id: string;
+			};
+		};
 	};
 
 	type appInfo = {
-		systemCode: typeof systemCode,
-		processType: typeof processType,
-		commitHash: typeof commitHash,
-		environment: typeof environment,
-		region: typeof region,
-		releaseDate: typeof releaseDate,
-		releaseVersion: typeof releaseVersion,
-		cloudProvider: typeof cloudProvider,
-		herokuAppId: typeof herokuAppId,
-		herokuDynoId: typeof herokuDynoId,
-		instanceId: typeof instanceId,
-		semanticConventions: SemanticConventions
+		systemCode: typeof systemCode;
+		processType: typeof processType;
+		commitHash: typeof commitHash;
+		environment: typeof environment;
+		region: typeof region;
+		releaseDate: typeof releaseDate;
+		releaseVersion: typeof releaseVersion;
+		cloudProvider: typeof cloudProvider;
+		herokuAppId: typeof herokuAppId;
+		herokuDynoId: typeof herokuDynoId;
+		instanceId: typeof instanceId;
+		semanticConventions: SemanticConventions;
 	};
 
 	export default appInfo;
