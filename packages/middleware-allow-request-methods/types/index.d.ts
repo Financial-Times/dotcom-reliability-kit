6fe1f01b09940ef304c34e489934d2bd74aecdf2
CyntiBinti <cynthia.mbulu@ft.com>
2025-02-11 14:38:31 +0000

    chore: refactor code and declare import type for module

--- packages/middleware-allow-request-methods/types/index.d.ts
+++ packages/middleware-allow-request-methods/types/index.d.ts
@@ -1,1 +1,11 @@
-declare module '@dotcom-reliability-kit/middleware-allow-request-methods' {}
+import { RequestHandler } from 'express';
+
+declare module '@dotcom-reliability-kit/middleware-allow-request-methods' {
+	export type RequestMethodOptions = {
+		allowedMethods?: string[];
+	};
+
+	declare function allowRequestMethods(
+		options?: RequestMethodOptions
+	): RequestHandler;
+}
