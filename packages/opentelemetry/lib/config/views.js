36d8d63e082e0008ba7918fcc43ea7f9ff9a0368
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2025-09-23 14:37:22 +0100

    refactor: reduce repetition when defining buckets

--- packages/opentelemetry/lib/config/views.js
+++ packages/opentelemetry/lib/config/views.js
@@ -4,6 +4,7 @@ const logger = require('@dotcom-reliability-kit/logger');
 
 /**
  * @import { NodeSDKConfiguration } from '@opentelemetry/sdk-node'
+ * @import { ViewOptions as OpenTelemetryView } from '@opentelemetry/sdk-metrics'
  * @import { ViewOptions } from '@dotcom-reliability-kit/opentelemetry'
  */
 
@@ -17,57 +18,48 @@ exports.createViewConfig = function createViewConfig({
 	httpClientDurationBuckets,
 	httpServerDurationBuckets
 }) {
-	/** @type {Partial<NodeSDKConfiguration>} */
-	const config = {};
-
-	if (
-		Array.isArray(httpClientDurationBuckets) &&
-		httpClientDurationBuckets?.length
-	) {
-		if (httpClientDurationBuckets.every(isPositiveNumber)) {
-			config.views ??= [];
-			config.views.push({
-				instrumentName: 'http.client.duration',
-				instrumentType: InstrumentType.HISTOGRAM,
-				aggregation: {
-					type: AggregationType.EXPLICIT_BUCKET_HISTOGRAM,
-					options: { boundaries: httpClientDurationBuckets }
-				}
-			});
-		} else {
-			logger.warn({
-				event: 'OTEL_VIEW_CONFIG_ISSUE',
-				message:
-					'HTTP client duration buckets must only contain positive numbers'
-			});
-		}
-	}
-
-	if (
-		Array.isArray(httpServerDurationBuckets) &&
-		httpServerDurationBuckets?.length
-	) {
-		if (httpServerDurationBuckets.every(isPositiveNumber)) {
-			config.views ??= [];
-			config.views.push({
-				instrumentName: 'http.server.duration',
-				instrumentType: InstrumentType.HISTOGRAM,
-				aggregation: {
-					type: AggregationType.EXPLICIT_BUCKET_HISTOGRAM,
-					options: { boundaries: httpServerDurationBuckets }
-				}
-			});
-		} else {
-			logger.warn({
-				event: 'OTEL_VIEW_CONFIG_ISSUE',
-				message:
-					'HTTP server duration buckets must only contain positive numbers'
-			});
-		}
-	}
-
-	return config;
+	const views = [
+		...buildHistogramView({
+			instrumentName: 'http.client.duration',
+			boundaries: httpClientDurationBuckets,
+			errorMessage:
+				'HTTP client duration buckets must only contain positive numbers'
+		}),
+		...buildHistogramView({
+			instrumentName: 'http.server.duration',
+			boundaries: httpServerDurationBuckets,
+			errorMessage:
+				'HTTP server duration buckets must only contain positive numbers'
+		})
+	];
+	return views.length ? { views } : {};
 };
+
+/**
+ * @param {object} options
+ * @param {string} options.instrumentName
+ * @param {number[] | undefined} options.boundaries
+ * @param {string} options.errorMessage
+ * @returns {OpenTelemetryView[]}
+ */
+function buildHistogramView({ instrumentName, boundaries, errorMessage }) {
+	if (Array.isArray(boundaries) && boundaries?.length) {
+		if (boundaries.every(isPositiveNumber)) {
+			return [
+				{
+					instrumentName,
+					instrumentType: InstrumentType.HISTOGRAM,
+					aggregation: {
+						type: AggregationType.EXPLICIT_BUCKET_HISTOGRAM,
+						options: { boundaries }
+					}
+				}
+			];
+		}
+		logger.warn({ event: 'OTEL_VIEW_CONFIG_ISSUE', message: errorMessage });
+	}
+	return [];
+}
 
 /**
  * @param {any} value
