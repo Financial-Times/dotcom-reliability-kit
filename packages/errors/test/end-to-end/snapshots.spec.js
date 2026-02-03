cadf47d8072d72716404c9662d543b706508f5a6
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2026-02-03 09:19:56 +0000

    test: fix the snapshot tests for errors
    These were not actually testing anything previously, and now we're being
    more explicit about which error properties we test. This fixes the
    snapshot tests.

--- packages/errors/test/end-to-end/snapshots.spec.js
+++ packages/errors/test/end-to-end/snapshots.spec.js
@@ -6,9 +6,21 @@ describe('@dotcom-reliability-kit/errors end-to-end', () => {
 	for (const [name, error] of Object.entries(errors)) {
 		describe(name, () => {
 			it('matches the snapshot', (test) => {
-				const expectedError = { ...error, message: error.message };
+				const expectedError = {
+					name: error.name,
+					isOperational: error.isOperational,
+					code: error.code,
+					data: error.data,
+					relatesToSystems: error.relatesToSystems,
+					statusCode: error.statusCode,
+					statusMessage: error.statusMessage,
+					message: error.message
+				};
 				if (error.cause) {
-					expectedError.cause = error.cause;
+					expectedError.cause = {
+						message: error.cause.message,
+						name: error.cause.name
+					};
 				}
 				test.assert.snapshot(expectedError);
 			});
