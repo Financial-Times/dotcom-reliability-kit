42a89db060825071e7b60296527ee41516fddda3
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2026-02-18 12:43:57 +0000

    refactor!: migrate serialize-request to ESM

--- packages/log-error/lib/index.js
+++ packages/log-error/lib/index.js
@@ -1,7 +1,7 @@
 const appInfo = require('@dotcom-reliability-kit/app-info');
 const reliabilityKitLogger = require('@dotcom-reliability-kit/logger');
 const { default: serializeError } = require('@dotcom-reliability-kit/serialize-error');
-const serializeRequest = require('@dotcom-reliability-kit/serialize-request');
+const { default: serializeRequest } = require('@dotcom-reliability-kit/serialize-request');
 
 /**
  * @import { ErrorLoggingOptions } from '@dotcom-reliability-kit/log-error'
