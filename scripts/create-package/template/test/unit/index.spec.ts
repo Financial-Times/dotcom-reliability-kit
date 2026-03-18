52c0f9a1863fcaac442d4cd60579b7da06f632a8
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2026-03-18 16:06:07 +0000

    refactor: migrate create-package to TypeScript
    This also makes it much nicer, similar to Camille's script for creating
    Client Metrics namespaces.

---
+++ scripts/create-package/template/test/unit/index.spec.ts
@@ -0,0 +1,8 @@
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+describe('@dotcom-reliability-kit/{{name}}', () => {
+	it('has some tests', () => {
+		assert.strictEqual(true, false);
+	});
+});
